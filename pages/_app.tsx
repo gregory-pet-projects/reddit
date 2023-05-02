0b64bfeed2372fd9c84684e91c04b4fc69ffcac0
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-02 23:38:34 +0300

    added toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 import client from "../apollo-client";
 
 export default function App({
@@ -12,6 +13,7 @@ export default function App({
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
+        <Toaster />
         <div className="h-screen overflow-y-scroll bg-slate-200">
           <Header />
           <Component {...pageProps} />
