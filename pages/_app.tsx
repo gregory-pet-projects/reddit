779415f3d5ad9c5b15128e59ea7a978490b025d8
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-02 18:34:19 +0300

    connected apollo to client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import client from "../apollo-client";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <div className="h-screen overflow-y-scroll bg-slate-200">
-        <Header />
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <div className="h-screen overflow-y-scroll bg-slate-200">
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
