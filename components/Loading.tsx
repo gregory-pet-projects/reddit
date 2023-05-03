1ae41ce21af831dcd728ff6cddd861fa8fac062b
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-03 14:12:12 +0300

    fixed styles

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ import { BeatLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center mt-20 h-screen">
+    <div className="flex w-full justify-center mt-20 h-screen">
       <BeatLoader color="gray" loading={true} />
     </div>
   );
